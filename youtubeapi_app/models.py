93372e00a17c319fe2963e4e29f2529b8b9261e2
marcos5163 <marcoschr56@gmail.com>
2023-09-12 13:22:41 +0530

    db_schema and migrations

--- youtubeapi_app/models.py
+++ youtubeapi_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+    """
+    Video model for storing video related data, indexing fields that are commanly used queries.
+    """
+    video_id = models.CharField(max_length=50, unique=True, db_index=True)  
+    title = models.CharField(max_length=200, db_index=True) 
+    description = models.TextField(db_index=True) 
+    published_datetime = models.DateTimeField(db_index=True)
+    thumbnail_url = models.URLField()
+
+    def __str__(self):
+        return self.title
